2ef1e810137d1ae329f7b09ae6351f45ad923b9a
krishna <Krishnayadav48065@gmail.com>
2024-03-07 18:44:34 +0530

    LogIn end point api complete

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,19 +1,35 @@
 const User=require('../models/user')
 const {StatusCodes} =require('http-status-codes')
-
+const {BadRequestError,UnauthenticatedError}=require('../errors')
 const register=async(req,res)=>{
 
-        const user = await User.create({...req.body});
-        const token=user.createJWT()
-        res.status(StatusCodes.CREATED).json({user:{name:user.name},token});
-   
+    const user = await User.create({...req.body});
+    const token=user.createJWT()
+    res.status(StatusCodes.CREATED).json({user:{name:user.name},token}); 
 }
 const signIn=async(req,res)=>{
     const {email,password}=req.body;
-    if(email===''||password===''){
-        res.status
+    if(!email || !password){
+        throw new BadRequestError('plz provide complete credentials')
     }
-    res.send('sign in route');
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+  
+  //compare password
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      name: user.name,
+      token,
+    },
+  });
 }
 
 module.exports={register,signIn}
