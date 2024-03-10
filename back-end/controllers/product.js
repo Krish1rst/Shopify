ffe36f93112f926a0854e1d9cbd8d511c5e58376
krishna <Krishnayadav48065@gmail.com>
2024-03-11 01:38:12 +0530

    Guest User Functionality done

--- back-end/controllers/product.js
+++ back-end/controllers/product.js
@@ -1,20 +1,19 @@
 const model=require('../models/product')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, NotFoundError } = require('../errors')
+
 
 const getOrders=async (req,res)=>{
     const order= await model.find({createdBy:req.user.userId}).sort({ createdAt: -1 })
     res.status(StatusCodes.OK).json({order,count:order.length})
 }
-const getAllOrders=async(req,res)=>{
-    const order=await model.find({});
-    res.status(StatusCodes.OK).json({order,count:order.length})
-}
+// const getAllOrders=async(req,res)=>{
+//     const order=await model.find({});
+//     res.status(StatusCodes.OK).json({order,count:order.length})
+// }
 const createOrder=async(req,res)=>{
     req.body.createdBy=req.user.userId
     const order=await model.create(req.body)
     res.status(StatusCodes.CREATED).json({order})
-    // res.status(200).send('ok from here')
 }
 
-module.exports={createOrder,getOrders,getAllOrders}
+module.exports={createOrder,getOrders}
